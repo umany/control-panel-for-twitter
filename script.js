53ecc9f31b8dd027e93c81cff11c41557f9228a1
umany <1769962+umany@users.noreply.github.com>
2024-02-01 00:23:54 +0900

    Fix unavailable retweets are missing on the profile timeline

--- script.js
+++ script.js
@@ -4701,6 +4701,8 @@ function shouldHideProfileTimelineItem(type) {
       return config.hideProfileRetweets
     case 'UNAVAILABLE_QUOTE_TWEET':
       return config.hideUnavailableQuoteTweets
+    case 'UNAVAILABLE_RETWEET':
+      return config.hideUnavailableQuoteTweets || config.hideProfileRetweets
     default:
       return true
   }
